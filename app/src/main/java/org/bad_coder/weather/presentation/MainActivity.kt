6d4eac81da2a7d96760cd42232a63165da05dfbd
Md. Mahfujur Rahman Arif <arif.lu.cse@gmail.com>
2023-01-17 02:53:43 +0100

    Create a new empty project

---
+++ app/src/main/java/org/bad_coder/weather/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.bad_coder.weather.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.bad_coder.weather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
